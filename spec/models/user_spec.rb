a21ff4a20774147bff2d9e30bf663cb5b9766265
JAY KIM <JAYHKIM23@GMAIL.COM>
2019-07-30 14:43:27 -0400

    devise authentication

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,35 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let!(:user) { User.new(first_name: "Lionel", last_name: "Messi", email: "Messi@barcelona.com", password: "password") }
+
+  describe "first_name" do
+  it "should return the user's first name" do
+    expect(user.first_name).to eq("Lionel")
+    end
+  end
+
+  describe "last_name" do
+  it "should return the user's last name" do
+    expect(user.last_name).to eq("Messi")
+    end
+  end
+
+  describe "username" do
+  it "should return the user's username" do
+    expect(user.username).to eq("Leo10")
+    end
+  end
+
+  describe "email" do
+  it "should return the user's email" do
+    expect(user.email).to eq("Messi@barcelona.com")
+    end
+  end
+
+  describe "password" do
+  it "should return the user's password" do
+    expect(user.password.length).to be >= 6
+    end
+  end
 end
