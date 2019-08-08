ae165e266dcad90c36c341840e5e3e0a11cfe68a
JAY KIM <JAYHKIM23@GMAIL.COM>
2019-08-08 18:20:03 -0400

    fixed the training tile styling

--- app/javascript/react/components/TrainingTile.js
+++ app/javascript/react/components/TrainingTile.js
@@ -5,13 +5,12 @@ const TrainingTile = (props) => {
 
   return (
 
-    <div className="callout" data-closable>
+    <div className="training-tile" data-closable>
+    <button className="close-button" aria-label="Close alert" type="button" onClick={() => props.handleDelete(props.id)} data-close>
+      <i className="glyphicon glyphicon-remove"></i>
+    </button>
         <Link to={`/trainings/${props.id}`}>
-          <h3 className="training-tile">
-          <button className="close-button" aria-label="Close alert" type="button" onClick={() => props.handleDelete(props.id)} data-close>
-            <i className="glyphicon glyphicon-remove"></i>
-          </button>
-          {props.title} | {props.date} | {props.time}</h3>
+          <h3 className="training-header"> {props.title} | {props.date} | {props.time}</h3>
         </Link>
     </div>
   )
